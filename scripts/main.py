7fc625520f0cdd81faf356b69c193d5fc3007504
Killian Haelman <31949@edu.campusdetandem.be>
2025-10-22 14:46:16 +0200

    Replaced main.py with what was previously ui.py
    Added support for file uploading in the UI version, so now that the UI also supports uploading files, i didnt see any reason for keeping the original shell based main.py around.

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,56 @@
+import tkinter as tk
+from tkinter import filedialog
 from textwrap import wrap
 
-read_file_path = input("Please input the file path here, it should look something like this: 'C:/Users/yourName/yourFolder/yourFile.txt' please note you MUST use forward slashes in your file path or else it will not work\n")
 write_file_path = "output.txt"
-final_content = ""
 
-with open(read_file_path, 'r') as file: #Read the file and split it, using 75 "="s as a divider
-    content = file.read()
-    split_content = wrap(content, 256)
-    for i in split_content:
-        final_content += f"{i}\n"
-        final_content += '=' * 75
-        final_content += '\n'
+class ui:
+    def __init__(self):
+        self.file_path = ''
+        self.window = tk.Tk()
 
-with open(write_file_path, 'w') as file: #Write it to a new file, and confirm this to the user
-    file.write(final_content)
-    print(f"Your file has been succesfully split. You can find it under the name {write_file_path} in the minecraft-book-splitter folder!")
+        self.window.geometry("800x500")
+        self.window.title = "Minecraft Book Splitter"
+
+
+        self.ui_title = tk.Label(self.window, text="Minecraft book splitter", font=('Old English Text MT', 30))
+        self.ui_title.pack(padx=20, pady=20)
+
+        self.input_box = tk.Text(self.window, height=5, font=('arial', 11))
+        self.input_box.pack(padx=10, pady=10)
+
+        self.choosefile_button = tk.Button(self.window, text='Choose File', command=self.open_file)
+        self.choosefile_button.pack()
+
+        self.confirm_button = tk.Button(self.window, text='confirm', command=self.show_confirm_text)
+        self.confirm_button.pack()
+
+        self.confirm_label = tk.Label(self.window, text="Awaiting input", font=("arial", 11))
+        self.confirm_label.pack(padx=10,pady=10)
+
+        self.window.mainloop()
+    
+    def open_file(self):
+        self.file_path = filedialog.askopenfilename()
+        print(self.file_path)
+
+    def show_confirm_text(self):
+        self.final_content = ''
+        if len(self.file_path) > 0:
+            with open(self.file_path, 'r') as file:
+                content = file.read()
+            split_content = wrap(content, 256)
+        elif len(self.input_box.get("1.0",'end-1c')) > 0:
+           split_content = wrap(self.input_box.get("1.0",'end-1c'), 256)
+        else:
+            return
+        for i in split_content:
+            self.final_content += f"{i}\n"
+            self.final_content += '=' * 75
+            self.final_content += '\n'
+        with open(write_file_path, 'w') as file: #Write it to a new file, and confirm this to the user
+            file.write(self.final_content)
+            self.confirm_label.config(text=f"Your file has been succesfully split. You can find it under the name {write_file_path} in the minecraft-book-splitter folder!", fg="green")
+    
+        
+ui()
